e4792f85da8c0d208019f650abcf206718943d89
Travis Thurber <thurbertr@Traviss-MacBook-Pro.local>
2019-10-22 22:29:42 -0400

    BCrypt not finished

--- src/main/java/com/User.java
+++ src/main/java/com/User.java
@@ -1,8 +1,12 @@
 package ecinema;
+import java.security.SecureRandom;
+import java.security.MessageDigest;
+import java.lang.Object;
+
 
 abstract class User{
 
-    private String userID;
+    protected String userID;
     private String password;
 
 		public String getUserID(){
@@ -21,6 +25,21 @@ abstract class User{
     public void setPassword(String passwd){
         password = passwd;
 		}
+
+    public String hashPassword(String pass)
+    {
+	/*
+	SecureRandom random = new SecureRandom();
+	byte[] salt = new byte[16];
+	random.nextBytes(salt);
+	MessageDigest md = MessageDigest.getInstance("SHA-512");
+	md.update(salt);
+	byte[] hashedPassword = md.digest(passwordToHash.getBytes(StandardCharsets.UTF_8));
+	*/
+	String pw_hash = BCrypt.hashpw(pass, BCrypt.gensalt());
+	System.out.println(pw_hash);
+    }//hashPassword
+
 }
 
 
