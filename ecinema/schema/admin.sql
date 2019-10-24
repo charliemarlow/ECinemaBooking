1a1b9018c62c996c4515564c58a86d14a60b4a2d
charliemarlow <cwm36477@uga.edu>
2019-10-24 00:35:07 -0400

    updated login, register, and index to be templated. per issue 10, made username/email UNIQUE in schema

--- ecinema/schema/admin.sql
+++ ecinema/schema/admin.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS `admin`;
 
 CREATE TABLE `admin` (
   `admin_id` INTEGER PRIMARY KEY AUTOINCREMENT,
-  `username` varchar(100) NOT NULL,
+  `username` varchar(100) NOT NULL UNIQUE,
   `password` binary(64) NOT NULL
 )
 
--- ecinema/schema/customer.sql
+++ ecinema/schema/customer.sql
@@ -4,9 +4,9 @@ CREATE TABLE `customer` (
   `customer_id` INTEGER PRIMARY KEY AUTOINCREMENT,
   `first_name` varchar(100) DEFAULT NULL,
   `last_name` varchar(100) DEFAULT NULL,
-  `email` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL UNIQUE,
   `subscribe_to_promo` tinyint(1) DEFAULT NULL,
-  `username` varchar(100) NOT NULL,
+  `username` varchar(100) NOT NULL UNIQUE,
   `password` binary(64) NOT NULL,
   `status` TEXT CHECK( status IN ('active', 'inactive', 'suspended') ) NOT NULL DEFAULT 'active'
 )
