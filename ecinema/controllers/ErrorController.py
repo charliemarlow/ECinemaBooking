253718744867c66e867f62773b2ee7e403c62b8d
Charlie <charles.w.marlow@gmail.com>
2019-10-28 21:25:04 -0400

    removed error controller

--- ecinema/controllers/ErrorController.py
+++
@@ -1,8 +0,0 @@
-from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for, Flask
-)
-
-bp = Blueprint('ErrorController', __name__, url_prefix='/')
-
-app = Flask(__name__)
-
