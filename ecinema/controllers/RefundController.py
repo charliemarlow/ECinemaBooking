1e013d4d8cd8c527bd6289dbae01078d060523ad
Charlie <charles.w.marlow@gmail.com>
2019-11-30 11:04:46 -0500

    defined template format for refunds in RefundController and orders.html

--- ecinema/controllers/RefundController.py
+++ ecinema/controllers/RefundController.py
@@ -21,8 +21,42 @@ from ecinema.models.Booking import Booking
 
 bp = Blueprint('RefundController', __name__, url_prefix='/')
 
+def refund(bid):
+    # clean up
+    # delete all tickets, reset showtime incrementer
+    # delete the booking object
+    return
 
 @bp.route('/previous_orders', methods=('GET', 'POST'))
 @customer_login_required
 def previous_orders():
-    return render_template('orders.html')
+    # need to get all booking objects for the customer
+    customer = Customer()
+    if not customer.fetch(g.user['username']):
+        return redirect(url_for('IndexController.index'))
+
+    if request.method == 'POST':
+        refund = request.form.get('refund')
+        details = request.form.get('view_details')
+
+        if refund:
+            print("Refund = " + refund)
+        if details:
+            print("details = " + details)
+
+
+
+    # then fill this out
+    refund_info = {
+        'movie_title' : 'test',
+        'order_no' : '#AF13J',
+        'date' : datetime.datetime.now(),
+        'tickets' : '2 x Adults, 1 x Child',
+        'total' : '$19.36 USD',
+        'bid' : 0, # or None if no longer available
+    }
+    refunds = []
+    refunds.append(refund_info)
+    flash('Your refund request was successful. Your payment provider will be fully refunded in 3-5 days.')
+    return render_template('orders.html', refunds=refunds)
+
